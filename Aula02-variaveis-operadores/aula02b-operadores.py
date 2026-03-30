bce88a929d287c829965b01b3837a47d1af1ffa3
gu-gz <gugzgz@gmail.com>
2026-03-30 20:47:58 -0300

    Aula sem os exercicios

---
+++ Aula02-variaveis-operadores/aula02b-operadores.py
@@ -0,0 +1,32 @@
+from operator import truediv
+
+num1 = 5
+num2 = 2
+
+print (type(num1), type(num2))
+
+resultado_operacao = num1 = num2
+print (resultado_operacao, type (resultado_operacao))
+
+#operadores de atribuição
+num = 15
+print() #pular linha
+print(num)
+
+num=num + 2 #acumulando 2 em num
+print(num)
+
+num -= 2
+print(num)
+
+#operadores relacionais
+print (6 >= 6)
+
+idade = 20
+print(idade >= 21) #booleano
+
+logado = True
+print(logado, type(logado))
+
+ maior_idade = idade >= 18
+ print(maior_idade, type(maior_idade))
