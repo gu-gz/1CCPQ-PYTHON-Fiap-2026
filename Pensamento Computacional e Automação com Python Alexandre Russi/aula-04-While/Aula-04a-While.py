0a5fc9733d4ade9c14c1bf04cccaee2b7a71e8c5
gu-gz <gugzgz@gmail.com>
2026-04-27 20:49:06 -0300

    aula do alexandre russi 27/04

---
+++ Pensamento Computacional e Automação com Python Alexandre Russi/aula-04-While/Aula-04a-While.py
@@ -0,0 +1,17 @@
+cp = 0
+while cp < 10:
+    cp += 1
+
+    if cp == 3 or cp == 5:
+        continue
+    if cp == 7:
+        break
+
+    print(f'produto {cp}')
+
+
+    #while decrescente de 4 ate 1
+i = 4
+while i > 0:
+    print (i)
+    i -= 1
---
+++ Pensamento Computacional e Automação com Python Alexandre Russi/aula-04-While/aula04-atividade03-For.py
@@ -0,0 +1,19 @@
+#for count_music in range(3):
+#   print(f"musica{count_music}")
+
+
+    #de 1 ate 11, pulando de 2 em 2
+    #for i in range(1, 12 , 2):
+    #print(i)
+
+    #atv 3
+    #qtd_musicas = int(input("digite a quantidade de musicas que voce tem na playlist (DB):"))
+
+    #for i in range(qtd_musicas):
+    #    print(f"musica {i}")
+
+#laços alinhados
+#rep encadeada
+for i in range(0, 4):
+    for j in range(0, 3, 2):
+        print(f"i:{i}, j:{j}")
